46dd332a8dc2c553c0272dc1bc4e9ee1552b2727
shoaibsaleem0299 <shoaib0299@gmail.com>
2024-03-06 17:48:39 +0500

    adding new  sections

--- src/component/App.js
+++ src/component/App.js
@@ -1,14 +1,25 @@
+import React from 'react';
 import '../styleSheet.css';
 import { LuGithub } from "react-icons/lu";
 import { FaLinkedinIn } from "react-icons/fa6";
 import { MdOutlineMailOutline } from "react-icons/md";
+import Typewriter from "./Typewriter";
+
+
 
 
 function App() {
+
+  const { text } = useTyp
+
   return (
-    
+
     <div class="startSection">
-      <h1 data-aos="fade-right" id='topHeading'>Software Engineer</h1>
+      <h1 id='topHeading'>Shoaib, <Typewriter text="software engineer" delay={100} />
+        {/* <span>
+          {text}
+        </span> */}
+      </h1>
       <ul className='socialLinks'>
         <li><a href="https://github.com/shoaibsaleem0299/"><LuGithub style={{ fontSize: '1.8em' }} /></a></li>
         <li><a href="https://linkedin.com/in/codewizshoaib/"><FaLinkedinIn style={{ fontSize: '1.8em' }} /></a></li>
